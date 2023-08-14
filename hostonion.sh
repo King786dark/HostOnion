35ef90c98147718aea1794b296d223f6979916c4
King786dark <106179042+King786dark@users.noreply.github.com>
2023-08-14 10:56:39 +0530

    Update hostonion.sh
    Index file is not download after tool run

--- hostonion.sh
+++ hostonion.sh
@@ -21,7 +21,7 @@ apt install php -y
 apt install nano
 rm index.html
 echo -e $'\e[1;33m[\e[0m\e[1;77m HostOnion \e[0m\e[1;33m]\e[0m\e[1;32m ## Generating Webpage \e[0m'
-wget https://raw.githubusercontent.com/Deauthxploit/Nothing_much/main/index.html
+wget https://github.com/King786dark/HostOnion/blob/main/index.html
 echo -e $'\e[1;33m[\e[0m\e[1;77m HostOnion \e[0m\e[1;33m]\e[0m\e[1;32m # Enter Details to display on the Webpage \e[0m'
 echo ""
 #Enter Full Name
